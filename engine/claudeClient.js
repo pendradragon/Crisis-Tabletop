45ca5fa88e4097baa8854838cd6e50771448e535
Megan Kerstetter (RIT Student) <mhk4706@rit.edu>
2026-08-26 15:25:21 -0400

    Just some formatting stuff before I go to class.

--- engine/claudeClient.js
+++ engine/claudeClient.js
@@ -33,13 +33,13 @@ const TURN_TOOL{
                     affected_areas: { type: "array", items: { type: "string" } },
                 },
                 required: [
-          "elapsed_time",
-                "severity_trend",
-                "media_attention",
-                "operational_impact",
-                "affected_areas",
-        ],
-      },
+                    "elapsed_time",
+                    "severity_trend",
+                    "media_attention",
+                    "operational_impact",
+                    "affected_areas",
+                ],
+            },
 decision_prompt: {
     type: "string",
         description:
